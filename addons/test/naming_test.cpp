46beffb3055965e241c6048d64ca75de979264d2
firewave <firewave@users.noreply.github.com>
2022-09-21 17:02:53 +0200

    naming_test.cpp: added test for private members variables

--- addons/test/naming_test.cpp
+++ addons/test/naming_test.cpp
@@ -2,6 +2,8 @@
 // ~/cppcheck/cppcheck --dump naming_test.cpp && python3 ../naming.py --var='[a-z].*' --function='[a-z].*' --private-member-variable='m[A-Z].*' --const='[_a-zA-Z].*' naming_test.cpp.dump
 
 // No error for mismatching Constructor/Destructor names should be issued, they can not be changed.
+namespace test1
+{
 class TestClass1
 {
     TestClass1() {}
@@ -9,3 +11,14 @@ class TestClass1
     TestClass1(const TestClass1 &) {}
     TestClass1(TestClass1 &&) {}
 };
+}
+
+namespace test2
+{
+class A
+{
+private:
+    int intValue;
+    int mIntValue;
+};
+}
