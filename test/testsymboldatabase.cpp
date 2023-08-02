afc77d3829418f365876875373f25ff11f2481ca
firewave <firewave@users.noreply.github.com>
2023-08-02 16:21:41 +0200

    TestSymbolDatabase: removed some redundant default parameters

--- test/testsymboldatabase.cpp
+++ test/testsymboldatabase.cpp
@@ -5387,7 +5387,7 @@ private:
               "struct A { };\n"
               "static void make(external::ns::s ss) {\n"
               "  internal::ns1::make<A>(ss);\n"
-              "}\n", true);
+              "}\n");
         ASSERT_EQUALS("", errout_str());
     }
 
@@ -9256,7 +9256,7 @@ private:
               "    int func3();\n"
               "    int func4(int);\n"
               "    return func4(x);\n"
-              "}\n", true);
+              "}\n");
         ASSERT_EQUALS("", errout_str());
     }
 
