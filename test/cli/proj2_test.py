88060db84c01fd167163d9df88f49a23f6cfa1ce
firewave <firewave@users.noreply.github.com>
2024-01-12 14:08:50 +0100

    made `test/cli/proj2_test.py` location-independent / some cleanups / added TODOs

--- test/cli/proj2_test.py
+++ test/cli/proj2_test.py
@@ -5,6 +5,8 @@ import json
 import os
 from testutils import create_gui_project_file, cppcheck
 
+__script_dir = os.path.dirname(os.path.abspath(__file__))
+
 COMPILE_COMMANDS_JSON = 'compile_commands.json'
 
 ERR_A = ('%s:1:7: error: Division by zero. [zerodiv]\n' +
@@ -14,93 +16,152 @@ ERR_B = ('%s:1:7: error: Division by zero. [zerodiv]\n' +
          'x = 3 / 0;\n' +
          '      ^\n') % os.path.join('b', 'b.c')
 
+
 def realpath(s):
     return os.path.realpath(s).replace('\\', '/')
 
-def create_compile_commands():
-    j = [{'directory': realpath('proj2/a'), 'command': 'gcc -c a.c', 'file': 'a.c'},
-         {'directory': realpath('proj2'), 'command': 'gcc -c b/b.c', 'file': 'b/b.c'}]
-    with open('proj2/' + COMPILE_COMMANDS_JSON, 'wt') as f:
+
+def create_compile_commands(compdb_path):
+    if not os.path.isdir(compdb_path):
+        os.makedirs(compdb_path)
+    j = [{'directory': realpath(os.path.join(__script_dir, 'proj2', 'a')), 'command': 'gcc -c a.c', 'file': 'a.c'},
+         {'directory': realpath(os.path.join(__script_dir, 'proj2')), 'command': 'gcc -c b/b.c', 'file': 'b/b.c'}]
+    with open(os.path.join(compdb_path, COMPILE_COMMANDS_JSON), 'wt') as f:
         f.write(json.dumps(j))
 
-# Run Cppcheck from project path
-def cppcheck_local(args):
-    cwd = os.getcwd()
-    os.chdir('proj2')
-    ret, stdout, stderr = cppcheck(args)
-    os.chdir(cwd)
-    return ret, stdout, stderr
 
+# TODO: why is this missing the progress messages?
 def test_file_filter():
-    ret, stdout, stderr = cppcheck(['proj2/','--file-filter=proj2/a/*'])
+    ret, stdout, stderr = cppcheck(['proj2/', '--file-filter=proj2/a/*'], cwd=__script_dir)
     file1 = os.path.join('proj2', 'a', 'a.c')
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        'Checking %s: AAA...' % file1
+    ]
+    assert ret == 0, stdout
+
+    ret, stdout, stderr = cppcheck(['proj2/', '--file-filter=proj2/b*'], cwd=__script_dir)
     file2 = os.path.join('proj2', 'b', 'b.c')
-    assert ret == 0
-    assert stdout.find('Checking %s ...' % file1) >= 0
-    ret, stdout, stderr = cppcheck(['proj2/','--file-filter=proj2/b*'])
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file2,
+    ]
     assert ret == 0, stdout
-    assert stdout.find('Checking %s ...' % file2) >= 0
 
-def test_local_path():
-    create_compile_commands()
-    ret, stdout, stderr = cppcheck_local(['--project=compile_commands.json'])
-    file1 = os.path.join('a', 'a.c')
-    file2 = os.path.join('b', 'b.c')
+
+def test_local_path(tmpdir):
+    create_compile_commands(tmpdir)
+    ret, stdout, stderr = cppcheck(['--project={}'.format(COMPILE_COMMANDS_JSON)], cwd=tmpdir)
+    file1 = os.path.join(__script_dir, 'proj2', 'a', 'a.c')
+    file2 = os.path.join(__script_dir, 'proj2', 'b', 'b.c')
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done'
+    ]
     assert ret == 0, stdout
-    assert stdout.find('Checking %s ...' % file1) >= 0
-    assert stdout.find('Checking %s ...' % file2) >= 0
 
-def test_local_path_force():
-    create_compile_commands()
-    ret, stdout, stderr = cppcheck_local(['--project=compile_commands.json', '--force'])
+
+# TODO: we actually need to trigger the --force limit
+def test_local_path_force(tmpdir):
+    create_compile_commands(tmpdir)
+    ret, stdout, stderr = cppcheck(['--project={}'.format(COMPILE_COMMANDS_JSON), '--force'], cwd=tmpdir)
+    file1 = os.path.join(__script_dir, 'proj2', 'a', 'a.c')
+    file2 = os.path.join(__script_dir, 'proj2', 'b', 'b.c')
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        'Checking %s: AAA...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done',
+    ]
     assert ret == 0, stdout
-    assert stdout.find('AAA') >= 0
 
-def test_local_path_maxconfigs():
-    create_compile_commands()
-    ret, stdout, stderr = cppcheck_local(['--project=compile_commands.json', '--max-configs=2'])
+
+# TODO: we actually need to hit the threshold
+def test_local_path_maxconfigs(tmpdir):
+    create_compile_commands(tmpdir)
+    ret, stdout, stderr = cppcheck(['--project={}'.format(COMPILE_COMMANDS_JSON), '--max-configs=2'], cwd=tmpdir)
+    file1 = os.path.join(__script_dir, 'proj2', 'a', 'a.c')
+    file2 = os.path.join(__script_dir, 'proj2', 'b', 'b.c')
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        'Checking %s: AAA...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done',
+    ]
     assert ret == 0, stdout
-    assert stdout.find('AAA') >= 0
 
-def test_relative_path():
-    create_compile_commands()
-    ret, stdout, stderr = cppcheck(['--project=proj2/' + COMPILE_COMMANDS_JSON])
-    file1 = os.path.join('proj2', 'a', 'a.c')
-    file2 = os.path.join('proj2', 'b', 'b.c')
+
+def test_relative_path(tmpdir):
+    create_compile_commands(os.path.join(tmpdir, 'proj2'))
+    ret, stdout, stderr = cppcheck(['--project={}'.format(os.path.join('proj2', COMPILE_COMMANDS_JSON))], cwd=tmpdir)
+    file1 = os.path.join(__script_dir, 'proj2', 'a', 'a.c')
+    file2 = os.path.join(__script_dir, 'proj2', 'b', 'b.c')
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done'
+    ]
     assert ret == 0, stdout
-    assert stdout.find('Checking %s ...' % file1) >= 0
-    assert stdout.find('Checking %s ...' % file2) >= 0
 
-def test_absolute_path():
-    create_compile_commands()
-    ret, stdout, stderr = cppcheck(['--project=' + os.path.realpath('proj2/' + COMPILE_COMMANDS_JSON)])
-    file1 = os.path.realpath('proj2/a/a.c')
-    file2 = os.path.realpath('proj2/b/b.c')
+
+def test_absolute_path(tmpdir):
+    create_compile_commands(tmpdir)
+    ret, stdout, stderr = cppcheck(['--project={}'.format(os.path.join(tmpdir, COMPILE_COMMANDS_JSON))])
+    file1 = os.path.realpath(os.path.join(__script_dir, 'proj2', 'a', 'a.c'))
+    file2 = os.path.realpath(os.path.join(__script_dir, 'proj2', 'b', 'b.c'))
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done'
+    ]
     assert ret == 0, stdout
-    assert stdout.find('Checking %s ...' % file1) >= 0
-    assert stdout.find('Checking %s ...' % file2) >= 0
 
-def test_gui_project_loads_compile_commands_1():
-    create_compile_commands()
-    ret, stdout, stderr = cppcheck(['--project=proj2/proj2.cppcheck'])
-    file1 = os.path.join('proj2', 'a', 'a.c')
-    file2 = os.path.join('proj2', 'b', 'b.c')
+
+# TODO
+def test_gui_project_loads_compile_commands_1(tmpdir):
+    create_compile_commands(os.path.join(tmpdir, 'proj2'))
+    ret, stdout, stderr = cppcheck(['--project={}'.format(os.path.join(__script_dir, 'proj2', 'proj2.cppcheck'))], cwd=tmpdir)
+    file1 = os.path.join(__script_dir, 'proj2', 'a', 'a.c')
+    file2 = os.path.realpath(os.path.join(__script_dir, 'proj2', 'b', 'b.c'))
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done'
+    ]
     assert ret == 0, stdout
-    assert stdout.find('Checking %s ...' % file1) >= 0
-    assert stdout.find('Checking %s ...' % file2) >= 0
 
-def test_gui_project_loads_compile_commands_2():
-    create_compile_commands()
+
+def test_gui_project_loads_compile_commands_2(tmpdir):
+    create_compile_commands(os.path.join(tmpdir, 'proj2'))
     exclude_path_1 = 'proj2/b'
-    create_gui_project_file('proj2/test.cppcheck',
-                            import_project='compile_commands.json',
+    create_gui_project_file(os.path.join(tmpdir, 'proj2', 'test.cppcheck'),
+                            import_project=COMPILE_COMMANDS_JSON,
                             exclude_paths=[exclude_path_1])
-    ret, stdout, stderr = cppcheck(['--project=proj2/test.cppcheck'])
-    file1 = os.path.join('proj2', 'a', 'a.c')
-    file2 = os.path.join('proj2', 'b', 'b.c') # Excluded by test.cppcheck
+    ret, stdout, stderr = cppcheck(['--project=proj2/test.cppcheck'], cwd=tmpdir)
+    file1 = os.path.join(__script_dir, 'proj2', 'a', 'a.c')
+    file2 = os.path.realpath(os.path.join(__script_dir, 'proj2', 'b', 'b.c'))
+    lines = stdout.splitlines()
+    assert lines == [
+        'Checking %s ...' % file1,
+        '1/2 files checked 50% done',
+        'Checking %s ...' % file2,
+        '2/2 files checked 100% done'
+    ]
     assert ret == 0, stdout
-    assert stdout.find('Checking %s ...' % file1) >= 0
-    assert stdout.find('Checking %s ...' % file2) < 0
 
 
 def test_gui_project_loads_relative_vs_solution():
