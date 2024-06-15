7f1eab898a2f746fa0bb89ab08d59b54ecd44f48
firewave <firewave@users.noreply.github.com>
2024-06-15 21:39:30 +0200

    suppressions.cpp: replaced `std::istringstream` usage with `strToInt<>()`

--- lib/suppressions.cpp
+++ lib/suppressions.cpp
@@ -234,8 +234,7 @@ std::string SuppressionList::addSuppressionLine(const std::string &line)
                 suppression.fileName.find('.', pos) == std::string::npos) {
                 // Try to parse out the line number
                 try {
-                    std::istringstream istr1(suppression.fileName.substr(pos+1));
-                    istr1 >> suppression.lineNumber;
+                    suppression.lineNumber = strToInt<int>(suppression.fileName.substr(pos+1));
                 } catch (...) {
                     suppression.lineNumber = SuppressionList::Suppression::NO_LINE;
                 }
