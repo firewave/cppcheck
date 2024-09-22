4405b72afcd4267646031e4f9df579dfeb511a37
firewave <firewave@users.noreply.github.com>
2024-09-22 20:59:47 +0200

    mitigated `bugprone-exception-escape` clang-tidy warnings

--- lib/tokenlist.cpp
+++ lib/tokenlist.cpp
@@ -1892,6 +1892,7 @@ namespace {
     struct OnException {
         std::function<void()> f;
 
+        // NOLINTNEXTLINE(bugprone-exception-escape) - reported with libc++ only
         ~OnException() {
 #ifndef _MSC_VER
 #if defined(__cpp_lib_uncaught_exceptions)
