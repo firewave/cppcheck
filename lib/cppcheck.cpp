bd51d4705117d33de786f38ecf44eec75f8656f3
firewave <firewave@users.noreply.github.com>
2024-09-30 10:38:34 +0200

    made some function names more descriptive

--- lib/cppcheck.cpp
+++ lib/cppcheck.cpp
@@ -799,7 +799,7 @@ unsigned int CppCheck::check(const FileWithDetails &file)
     return returnValue;
 }
 
-unsigned int CppCheck::check(const FileWithDetails &file, const uint8_t* data, std::size_t size)
+unsigned int CppCheck::checkBuffer(const FileWithDetails &file, const uint8_t* data, std::size_t size)
 {
     return checkBuffer(file, emptyString, 0, data, size);
 }
@@ -1078,7 +1078,7 @@ unsigned int CppCheck::checkInternal(const FileWithDetails& file, const std::str
                     code += "#line " + std::to_string(dir.linenr) + " \"" + dir.file + "\"\n" + dir.str + '\n';
             }
             TokenList tokenlist(mSettings, file.lang());
-            tokenlist.createTokens(code.data(), code.size()); // TODO: check result?
+            tokenlist.createTokensFromBuffer(code.data(), code.size()); // TODO: check result?
             executeRules("define", tokenlist);
         }
 #endif
