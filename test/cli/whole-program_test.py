20b0e2ab0a62c37787d8aeaa51edead8ad136502
firewave <firewave@users.noreply.github.com>
2024-12-11 13:36:25 +0100

    s

--- test/cli/whole-program_test.py
+++ test/cli/whole-program_test.py
@@ -8,9 +8,10 @@ from testutils import cppcheck
 __script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # TODO: use dedicated addon
-# TODO: test CheckNullPointer
-# TODO: test CheckUninitVar
-# TODO: test CheckBufferOverrun
+# TODO: test CheckNullPointer - ctunullpointer
+# TODO: test CheckUninitVar - ctuuninitvar
+# TODO: test CheckBufferOverrun - ctuArrayIndex
+# TODO: test CheckBufferOverrun - ctuPointerArith
 
 
 def __create_compile_commands(dir, entries):
