dd542224ef3162e79b21dd546aa0fe9a3eb628a6
firewave <firewave@users.noreply.github.com>
2024-12-30 02:42:13 +0100

    c

--- oss-fuzz/main.cpp
+++ oss-fuzz/main.cpp
@@ -58,7 +58,7 @@ static Settings create_settings()
 }
 static const Settings s_settings = create_settings();
 static DummyErrorLogger s_errorLogger;
-static const FileWithDetails s_file("test.cpp", Standards::Language::CPP, 0);
+static const FileWithDetails s_file("test.cpp", Standards::Language::C, 0);
 
 static void doCheck(const std::string& code)
 {
