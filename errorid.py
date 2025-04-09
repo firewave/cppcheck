102a0e6f927a0f5c383240ecd39b5a81901631b0
firewave <firewave@users.noreply.github.com>
2025-04-09 10:01:28 +0200

    temp

---
+++ errorid.py
@@ -0,0 +1,21 @@
+import subprocess
+import pathlib
+
+cmd = './testrunner TestOther'
+
+with subprocess.Popen(cmd.split(), stderr=subprocess.PIPE, universal_newlines=True) as p:
+    out = p.stderr.read().strip()
+
+file = '/home/user/CLionProjects/cppcheck/test/testother.cpp'
+
+text = pathlib.Path(file).read_text()
+
+lines = out.splitlines()
+for l in lines:
+    if not '0SEP0' in l:
+        continue
+    parts = l.split('0SEP0')
+    text = text.replace(parts[0], parts[1])
+
+with open(file, 'wt') as f:
+    f.write(text)
