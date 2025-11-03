0fad1cc2ee1f53355071b88cfd72d655f3d708f0
firewave <firewave@users.noreply.github.com>
2025-11-03 13:49:07 +0100

    forwardanalyzer.cpp: reduced scope of some variables

--- lib/forwardanalyzer.cpp
+++ lib/forwardanalyzer.cpp
@@ -615,13 +615,13 @@ namespace {
                 } else if (tok->str() == ";" && tok->astParent()) {
                     Token* top = tok->astTop();
                     if (Token::Match(top->previous(), "for|while (") && Token::simpleMatch(top->link(), ") {")) {
-                        Token* endCond = top->link();
-                        Token* endBlock = endCond->linkAt(1);
                         Token* condTok = getCondTok(top);
-                        Token* stepTok = getStepTok(top);
                         // The semicolon should belong to the initTok otherwise something went wrong, so just bail
                         if (tok->astOperand2() != condTok && !Token::simpleMatch(tok->astOperand2(), ";"))
                             return Break(Analyzer::Terminate::Bail);
+                        Token* endCond = top->link();
+                        Token* endBlock = endCond->linkAt(1);
+                        Token* stepTok = getStepTok(top);
                         if (updateLoop(end, endBlock, condTok, nullptr, stepTok) == Progress::Break)
                             return Break();
                     }
@@ -643,12 +643,11 @@ namespace {
                 } else if (tok->link() && tok->str() == "}" && tok == tok->scope()->bodyEnd) { // might be an init list
                     const Scope* scope = tok->scope();
                     if (contains({ScopeType::eDo, ScopeType::eFor, ScopeType::eWhile, ScopeType::eIf, ScopeType::eElse, ScopeType::eSwitch}, scope->type)) {
-                        const bool inElse = scope->type == ScopeType::eElse;
-                        const bool inDoWhile = scope->type == ScopeType::eDo;
-                        const bool inLoop = contains({ScopeType::eDo, ScopeType::eFor, ScopeType::eWhile}, scope->type);
                         Token* condTok = getCondTokFromEnd(tok);
                         if (!condTok)
                             return Break();
+                        const bool inElse = scope->type == ScopeType::eElse;
+                        const bool inLoop = contains({ScopeType::eDo, ScopeType::eFor, ScopeType::eWhile}, scope->type);
                         if (!condTok->hasKnownIntValue() || inLoop) {
                             if (!analyzer->lowerToPossible())
                                 return Break(Analyzer::Terminate::Bail);
@@ -674,6 +673,7 @@ namespace {
                             }
                         }
                         analyzer->assume(condTok, !inElse, Analyzer::Assume::Quiet);
+                        const bool inDoWhile = scope->type == ScopeType::eDo;
                         assert(!inDoWhile || Token::simpleMatch(tok, "} while ("));
                         if (Token::simpleMatch(tok, "} else {") || inDoWhile)
                             tok = tok->linkAt(2);
@@ -692,12 +692,12 @@ namespace {
                         }
                         return Break(Analyzer::Terminate::Bail);
                     }
-                    Token* endCond = tok->linkAt(1);
-                    Token* endBlock = endCond->linkAt(1);
-                    Token* condTok = getCondTok(tok);
                     Token* initTok = getInitTok(tok);
                     if (initTok && updateRecursive(initTok) == Progress::Break)
                         return Break();
+                    Token* endCond = tok->linkAt(1);
+                    Token* endBlock = endCond->linkAt(1);
+                    Token* condTok = getCondTok(tok);
                     if (Token::Match(tok, "for|while (")) {
                         // For-range loop
                         if (Token::simpleMatch(condTok, ":")) {
@@ -730,7 +730,6 @@ namespace {
                         std::tie(thenBranch.check, elseBranch.check) = evalCond(condTok);
                         if (!thenBranch.check && !elseBranch.check && stopOnCondition(condTok) && stopUpdates())
                             return Break(Analyzer::Terminate::Conditional);
-                        const bool hasElse = Token::simpleMatch(endBlock, "} else {");
                         bool bail = false;
 
                         // Traverse then block
@@ -745,6 +744,7 @@ namespace {
                                 bail = true;
                         }
                         // Traverse else block
+                        const bool hasElse = Token::simpleMatch(endBlock, "} else {");
                         if (hasElse) {
                             elseBranch.escape = isEscapeScope(endBlock->linkAt(2), elseBranch.escapeUnknown);
                             if (elseBranch.check) {
