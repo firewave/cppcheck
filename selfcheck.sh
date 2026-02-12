c01d204345c28031e967e12c82fd7786027822c0
firewave <firewave@users.noreply.github.com>
2026-02-12 17:05:56 +0100

    refs #13245 - use builddir consistently and verify usage of cached results in selfcheck

--- selfcheck.sh
+++ selfcheck.sh
@@ -11,9 +11,6 @@ cppcheck_options="-D__CPPCHECK__ -DCHECK_INTERNAL -DHAVE_RULES --library=cppchec
 qt_options="--library=qt -DQT_VERSION=0x060000 -DQ_MOC_OUTPUT_REVISION=68 -DQT_MOC_HAS_STRINGDATA"  # TODO: use 69 as revision
 naming_options="--addon-python=$(command -v python) --addon=naming.json"
 
-mkdir_cmd=$(command -v mkdir)
-rm_cmd=$(command -v rm)
-
 # clear PATH to prevent unintentional process invocations
 export PATH=
 
@@ -23,28 +20,21 @@ $cppcheck_bin $selfcheck_options \
   externals \
   || ec=1
 
-$mkdir_cmd b1
-
 $cppcheck_bin $selfcheck_options $cppcheck_options $naming_options \
-  --cppcheck-build-dir=b1 \
   frontend \
   || ec=1
 
 $cppcheck_bin $selfcheck_options $cppcheck_options $naming_options \
-  --cppcheck-build-dir=b1 \
   -Ifrontend \
   cli \
   || ec=1
 
 $cppcheck_bin $selfcheck_options $cppcheck_options $naming_options \
-  --cppcheck-build-dir=b1 --enable=internal \
+  --enable=internal \
   lib \
   || ec=1
 
-$mkdir_cmd b2
-
 $cppcheck_bin $selfcheck_options $cppcheck_options $naming_options $qt_options \
-  --cppcheck-build-dir=b2 \
   -DQT_CHARTS_LIB \
   -I$cmake_output/gui -Ifrontend -Igui \
   gui/*.cpp $cmake_output/gui \
@@ -64,8 +54,5 @@ $cppcheck_bin $selfcheck_options $cppcheck_options $qt_options \
   -I$cmake_output/tools/triage -Igui \
   tools/triage/*.cpp $cmake_output/tools/triage \
   || ec=1
-
-$rm_cmd -rf b2
-$rm_cmd -rf b1
 
 exit $ec
