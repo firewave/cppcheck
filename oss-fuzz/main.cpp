b4c912b7bcc8d2b3118aea12b89e8e89526e6e84
firewave <firewave@users.noreply.github.com>
2026-07-07 09:23:34 +0200

    q

--- oss-fuzz/main.cpp
+++ oss-fuzz/main.cpp
@@ -66,7 +66,7 @@ static Settings create_settings()
     Settings s;
     s.templateFormat = "{bold}{file}:{line}:{column}: {red}{inconclusive:{magenta}}{severity}:{inconclusive: inconclusive:}{default} {message} [{id}]{reset}\\n{code}";
     s.templateLocation = "{bold}{file}:{line}:{column}: {dim}note:{reset} {info}\\n{code}";
-    s.quiet = true;
+    s.quiet = false;
     s.addEnabled("all");
     s.certainty.setEnabled(Certainty::inconclusive, true);
     s.checkLevel = Settings::CheckLevel::exhaustive;
